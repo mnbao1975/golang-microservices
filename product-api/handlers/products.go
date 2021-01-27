688aa2420753c761719077ad92c441b6a7624ed8
Bao Nguyen <bao.nguyen201275@gmail.com>
2021-01-27 12:43:13 +0700

    going to apply midleware

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -3,8 +3,9 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"regexp"
 	"strconv"
+
+	"github.com/gorilla/mux"
 
 	"github.com/mnbao1975/microservices/product-api/data"
 )
@@ -19,53 +20,10 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPost {
-		p.addProduct(rw, r)
-	}
-
-	if r.Method == http.MethodPut {
-		p.l.Println(r.RequestURI)
-		// Expect the id in the URI
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		grp := reg.FindAllStringSubmatch(r.RequestURI, -1)
-
-		if len(grp) != 1 {
-			p.l.Println("Invalid URI more that one id")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		if len(grp[0]) != 2 {
-			p.l.Println("Invalid URI more that one capture group")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		idString := grp[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		p.l.Println("PUT id: ", id)
-		p.updateProduct(id, rw, r)
-	}
-	// Catch all
-	rw.WriteHeader(http.StatusMethodNotAllowed)
-}
-
-// GET method
-func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+// GetProducts return list of products
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("GET products")
 	lp := data.GetProducts()
-	//d, err := json.Marshal(lp)
 
 	// Stream the JSON to client, no need to complete parsing it and send to client
 	err := lp.ToJSON(rw)
@@ -77,8 +35,8 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	//rw.Write(d)
 }
 
-// POST method
-func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+// AddProduct will add a new product
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("POST a product")
 
 	prod := &data.Product{}
@@ -93,12 +51,20 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	data.AddProduct(prod)
 }
 
-// PUT method
-func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("PUT a product")
+// UpdateProduct will update a product by id
+func (p *Products) UpdateProduct(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "Invalid id", http.StatusBadRequest)
+		return
+	}
+
+	p.l.Println("PUT a product: ", id)
+
 	prod := &data.Product{}
 
-	err := prod.FromJSON(r.Body)
+	err = prod.FromJSON(r.Body)
 	if err != nil {
 		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
 		return
--- product-api/main.go
+++ product-api/main.go
@@ -9,6 +9,7 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/mnbao1975/microservices/product-api/handlers"
 )
 
@@ -16,11 +17,16 @@ func main() {
 	l := log.New(os.Stdout, "production-api: ", log.LstdFlags)
 
 	ph := handlers.NewProducts(l)
-	gb := handlers.NewGoodBye(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
-	sm.Handle("/goodbye", gb)
+	sm := mux.NewRouter()
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProduct)
 
 	s := &http.Server{
 		Addr:         ":3000",
