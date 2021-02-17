5d95e1bda1ef9d54583e6f232d676fd012e9e37e
Bao Nguyen <bao.nguyen201275@gmail.com>
2021-02-17 23:36:36 +0700

    working on uploading file

--- product-images/handlers/files.go
+++ product-images/handlers/files.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"github.com/mnbao1975/microservices/product-images/files"
@@ -20,15 +21,32 @@ func NewFiles(s files.Storage, l *log.Logger) *Files {
 	return &Files{log: l, store: s}
 }
 
-func (f *Files) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (f *Files) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	fn := vars["filename"]
 
 	f.log.Printf("POST - id: %s and filename: %s\n", id, fn)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]interface{}{
+
+	// no need to check for invalid id or filename as the mux router will not send requests
+	// here unless they have the correct parameters
+	f.saveFile(id, fn, rw, r)
+}
+
+// saveFile saves the contents of the request to a file
+func (f *Files) saveFile(id, path string, rw http.ResponseWriter, r *http.Request) {
+	f.log.Printf("Save file for product id (%s) and path (%s)\n", id, path)
+
+	fp := filepath.Join(id, path)
+	err := f.store.Save(fp, r.Body)
+	if err != nil {
+		f.log.Println("Unable to save file:", err)
+		http.Error(rw, "Unable to save file", http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(map[string]interface{}{
 		"message": "file was saved",
 	})
-
 }
